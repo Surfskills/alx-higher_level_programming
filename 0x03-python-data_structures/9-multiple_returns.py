45bfb8ddd143971635a1e4a7edb7ee30cca12556
Surfskills <ojjfred@gmail.com>
2022-06-06 18:54:42 +0300

    Data structures

--- 0x03-python-data_structures/9-multiple_returns.py
+++ 0x03-python-data_structures/9-multiple_returns.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list = [0]:
-    for i in range len(my_list):
-        max = i > len(my_list)
-    return max
-    else:
-        
-    return none
+    if len(my_list) == 0:
+        return (None)
+
+    big = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > big:
+            big = my_list[i]
+
+    return (big)
