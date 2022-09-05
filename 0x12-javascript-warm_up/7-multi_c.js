a2af106c18f0d5293baab620d67075d9c75ea88b
Surfskills <ojjfred@gmail.com>
2022-09-05 21:18:07 +0300

    Multiple C is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
+'use strict';
 let x = process.argv[2];
-if (isNaN(X)){
-    console.log("Missing number of occurrences");
-}   else {
-    for(let i=o;i<x;i++){
-	console.log("C is fun");
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
