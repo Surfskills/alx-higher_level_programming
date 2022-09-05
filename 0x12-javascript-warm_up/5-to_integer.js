2aeb84aad355665e9c280943244d31610624b7f1
Surfskills <ojjfred@gmail.com>
2022-09-05 20:56:19 +0300

    Multiple languages

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 
 #!/usr/bin/node
-'use strict';
 let arg = process.argv[2];
 if (isNaN(arg)) {
-  console.log('Not a number');
+  console.log("Not a number");
 } else {
-  console.log('My number: ' + arg);
+  console.log("My number: ${arg}");
 }
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+let args = (C is fun\nPython is cool\nJavaScript is amazing")
+for (let arg in args){
+   console.log(args[arg]);
+}
