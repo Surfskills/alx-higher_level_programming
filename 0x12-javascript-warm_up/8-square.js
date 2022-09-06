e6bae23e67e91d6de1f2dbb5a86d574cde8850cc
Surfskills <ojjfred@gmail.com>
2022-09-06 11:32:13 +0300

    Another

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,6 +4,7 @@ let x = process.argv[2];
 if (isNaN(x)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < x; i++) {
-    console.log('X'.repeat(x));
+    for (let i = 0; i < x; i++) {
+	for (let j = 0; j < x, j++)
+	    console.log('X'.repeat(x));
 }
