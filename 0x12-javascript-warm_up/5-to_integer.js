6955c6501fa30c7978d942fb97802446b2540a6e
Surfskills <ojjfred@gmail.com>
2022-09-06 09:40:15 +0300

    Revision

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 #!/usr/bin/node
 let arg = process.argv[2];
 if (isNaN(arg)) {
-  console.log("Not a number");
+  console.log('Not a number');
 } else {
-  console.log("My number: ${arg}");
+  console.log('My number: ${arg}');
 }
