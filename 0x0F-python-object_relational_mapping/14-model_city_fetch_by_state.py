2d9aa4048c201339274aefd27ad273d0a0b5125c
Surfskills <ojjfred@gmail.com>
2022-09-11 15:46:57 +0300

    Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -3,19 +3,21 @@
     the database hbtn_0e_14_usa """
 
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.schema import Table
+
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    engine = create_engine(f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}\
+                            @localhost/{sys.argv[3]}", pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    for city, state in session.query(City, State) \
-                              .filter(City.state_id == State.id) \
-                              .order_by(City.id):
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session = Session(engine)
+    for state, city in session.query(State, City)\
+                              .filter(City.state_id == State.id)\
+                              .order_by(City.id).all():
+        print(f"{state.name}: ({city.id}) {city.name}")
+    session.close()
